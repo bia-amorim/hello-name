2f132299caa3ff1f727a8a12b69b49875e7c8585
bia-amorim <biia.a.matos@gmail.com>
2020-11-24 16:06:41 -0300

    first commit

---
+++ src/main/java/br/edu/usj/lpii/helloname/HelloName.java
@@ -0,0 +1,24 @@
+package br.edu.usj.lpii.helloname;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HelloName {
+    
+    @PostMapping(value="helloName")
+    public ModelAndView postHelloName (@RequestParam String nome) {
+        
+        ModelAndView modelAndView = new ModelAndView("hello-name");
+        
+        String bemVindo = "Olá" + nome + " pessoa incrível da internet!";
+        modelAndView.addObject("mensagem", bemVindo);
+
+        return modelAndView;
+    }
+    
+}
+
